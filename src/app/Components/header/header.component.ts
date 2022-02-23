405d8b0a713df67e0a6d298a4bbe206abb990589
Santiago Correa <desarrollo@lacortedelosbuhos.com>
2022-02-22 23:13:50 -0500

    Menu solo admins

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  show=false;
+  params = new URLSearchParams(window.location.search)
   ngOnInit(): void {
+    if(!this.params.has("value")){
+      this.show=true
+    }
+    if(!sessionStorage.getItem("email")){
+      this.show=true
+    }
   }
 
 }
